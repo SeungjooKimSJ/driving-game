e4044f08c08d5ec6c7c598e95fd31a3846c23ba5
Seungjoo Kim <sjskybone@gmail.com>
2021-02-19 00:40:40 -0800

    Completed issue-2-turn-car. #2

--- main.js
+++ main.js
@@ -1,105 +1,14 @@
-// var $image = document.querySelector('.image');
-
-// $image.addEventListener('keydown', function (event) {
-
-//   switch(event.code) {
-//     case 'KeyD':
-//     case 'ArrowRight':
-//       break;
-
-//     case 'KeyS':
-//     case 'ArrowDown':
-//       break;
-
-//     case 'KeyA':
-//     case 'ArrowLeft':
-//       break;
-
-//     case 'KeyW':
-//     case 'ArrowUp':
-//       break;
-//   };
-
-
-//   if (event.key === 'ArrowRight') {
-//     $image.className = 'image';
-//   } else if (event.key === 'ArrowDown') {
-//     $image.className = 'image' + 'rotate90';
-//   } else if (event.key === 'ArrowLeft') {
-//     $image.className = 'image' + 'rotate180';
-//   } else if (event.key === 'ArrowUp') {
-//     $image.className = 'image' + 'rotate270';
-//   }
-
-// });
-
-
 var $image = document.querySelector('.image');
-console.log($image);
 
 document.addEventListener('keydown', function (event) {
 
-  console.log('key pressed!');
-  console.log('event.key:', event.key);
-
-  switch(event.code) {
-    case 'KeyD':
-    case 'ArrowRight':
-      break;
-
-    case 'KeyS':
-    case 'ArrowDown':
-      break;
-
-    case 'KeyA':
-    case 'ArrowLeft':
-      break;
-
-    case 'KeyW':
-    case 'ArrowUp':
-      break;
-  };
-
   if (event.key === 'ArrowRight') {
-    console.log('ArrowRight clicked');
     $image.className = 'image';
   } else if (event.key === 'ArrowDown') {
-    console.log('ArrowDown clicked');
-    $image.className = ('image' + 'rotate90');
+    $image.className = 'image rotate90';
   } else if (event.key === 'ArrowLeft') {
-    $image.className = 'image' + 'rotate180';
+    $image.className = 'image rotate180';
   } else if (event.key === 'ArrowUp') {
-    $image.className = 'image' + 'rotate270';
+    $image.className = 'image rotate270';
   }
 });
-
-
-
-// $image.addEventListener('keydown', function (event) {
-//   if (event.defaultPrevented) {
-//     return;
-//   };
-
-//   switch(event.code) {
-//     case 'KeyD':
-//     case 'ArrowRight':
-//       $image.className = 'image';
-//       break;
-
-//     case 'KeyS':
-//     case 'ArrowDown':
-//       $image.className = 'image' + 'rotate90';
-//       break;
-
-//     case 'KeyA':
-//     case 'ArrowLeft':
-//       $image.className = 'image' + 'rotate180';
-//       break;
-
-//     case 'KeyW':
-//     case 'ArrowUp':
-//       $image.className = 'image' + 'rotate270';
-//       break;
-//   }
-//   event.preventDefault();
-// }, true);
